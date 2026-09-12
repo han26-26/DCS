0428cf540eaca4847ab6e9c10ebddae1b3600311
han26-26 <thu555557@gmail.com>
2026-09-12 20:42:08 +0630

    Update sample.py

--- sample.py
+++ sample.py
@@ -189,7 +189,7 @@ st.info("💡 ဤနေရာမှ တဆင့် Google Sheet (Sheet2) သိ
 with st.form("add_question_form"):
                 new_q = st.text_area("မေးခွန်း (Question)")
                 col_a, col_b = st.columns(2)
-    with col_a:
+with col_a:
                     opt1 = st.text_input("Option A")
                     opt2 = st.text_input("Option B")
                 with col_b:
