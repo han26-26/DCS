d819eb64957eb57962f7d7faa65e4c2fdf7dd364
han26-26 <thu555557@gmail.com>
2026-09-13 06:16:46 +0630

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
-import streamlit as st
+if streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 import urllib.request
@@ -197,8 +197,8 @@ with col_b:
                     opt4 = st.text_input("Option D")
                 
 correct_ans = st.text_input("အမှန်ဖြေ (Correct Answer - အထက်ပါ Options များထဲမှ တစ်ခုအတိုင်း အတိအကျရေးပါ)")
-submitted_q = st.form_submit_button("Google Sheet သို့ မေးခွန်းအသစ် ထည့်မည်")
-             if submitted_q:
+submitted_q = st.form_submit_button("Google Sheet သို့ မေးခွန်းအသစ် ထည့်ထည့်မည်")
+if submitted_q:
                     if new_q and opt1 and opt2 and opt3 and opt4 and correct_ans:
                         try:
                             payload = json.dumps({
