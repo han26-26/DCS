b76a7caf97d38d0359c0e9cf328f60574eea5c60
han26-26 <thu555557@gmail.com>
2026-09-13 17:46:28 +0630

    Update sample.py

--- sample.py
+++ sample.py
@@ -278,7 +278,7 @@ if st.button("Final Submit & Lock Account", type="primary"):
                     st.session_state.submitted = True
                     st.session_state.final_score = score
                     st.rerun()
-else:
+                        else:
     st.warning("⚠️ မေးခွန်းများ Google Sheet ထဲတွင် မတွေ့ရှိရသေးပါ။ ကျေးဇူးပြု၍ Sheet2 ကို စစ်ဆေးပါ။")
 else:
             disp_score = st.session_state.final_score if 'final_score' in st.session_state else 0
